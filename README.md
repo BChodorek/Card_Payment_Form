6dd22e364cbc9005be4ad68b55db58ea8c8c0eb0
lewpustelnik <bartlomiej.chodorek@gmail.com>
2019-11-19 14:32:27 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Credit Card Form
+
+Demo available [here]().
+
+
+
+Credit card form with visual card and dynamic data representation.
+
+Done with HTML/CSS(SASS)/JS
+
+
+
---
+++ main.js
@@ -0,0 +1,48 @@
+const form = document.querySelector(".form");
+const cardNumber = document.querySelector(".cardNumber");
+const cardName = document.querySelector(".cardName");
+const cardCvv = document.querySelector(".cardCvv");
+const cardMonth = document.querySelector(".month");
+const cardYear = document.querySelector(".year");
+const numberMask = "################";
+
+const mapCardNumber = str => {
+  // console.log(str.split(""));
+  const html = str.split("").map((char, index) => {
+    if (index > 0 && index % 4 === 0) {
+      return `<span> </span><div class='numberItem'>${char}</div>`;
+    } else {
+      return `<div class='numberItem'>${char}</div>`;
+    }
+  });
+  cardNumber.innerHTML = html.join("");
+};
+
+const displayInput = e => {
+  const value = form[e.target.name].value.trim();
+  console.log(e.target.name);
+  if (e.target.name === "number" && value !== null) {
+    if (value === "") {
+      mapCardNumber(numberMask);
+    } else {
+      mapCardNumber(value);
+    }
+  } else if (e.target.name === "name") {
+    cardName.textContent = value;
+  } else if (e.target.name === "cvv") {
+    cardCvv.textContent = value;
+  } else if (e.target.name == "date--month") {
+    cardMonth.textContent = cardMonth.textContent.replace(/[^]*/, value);
+    console.log(cardMonth.textContent);
+    console.log(value);
+  } else if (e.target.name === "date--year") {
+    cardYear.textContent = cardYear.textContent.replace(
+      /[^]*/,
+      value.slice(2, 4)
+    );
+  }
+};
+
+mapCardNumber(numberMask);
+form.addEventListener("keyup", e => displayInput(e));
+form.addEventListener("change", e => displayInput(e));
