d119372f3c442005be1478ada0ec635fccd7c9e8
lewpustelnik <bartlomiej.chodorek@gmail.com>
2019-12-09 22:25:27 +0100

    Card number validation

--- main.js
+++ main.js
@@ -5,6 +5,43 @@ const cardCvv = document.querySelector(".cardCvv");
 const cardMonth = document.querySelector(".month");
 const cardYear = document.querySelector(".year");
 const numberMask = "################";
+const numberInput = form['number'];
+const invalidInfo = document.querySelector('.form--input__invalid-info');
+
+const validateCreditCardNumber = cardNumber => {
+
+	cardNumber = cardNumber.split(' ').join("");
+	if (parseInt(cardNumber) <= 0 || (!/\d{15,16}(~\W[a-zA-Z])*$/.test(cardNumber)) || cardNumber.length > 16) {
+    numberInput.classList.add('invalid')
+		return false;
+  }
+	var numArray = new Array();
+	for (var i = 0; i < cardNumber.length; i++) {
+    numArray[numArray.length] = cardNumber.charCodeAt(i) - 48;
+	}
+	numArray.reverse();
+	var sum = 0;
+	for (var i = 0; i < numArray.length; i++) {
+    var tmp = numArray[i];
+		if ((i % 2) != 0) {
+      tmp *= 2;
+			if (tmp > 9) {
+        tmp -= 9;
+			}
+		}
+		sum += tmp;
+  }
+
+  if ((sum % 10) != 0) {
+    numberInput.classList.remove('valid')
+    numberInput.classList.add('invalid')
+    invalidInfo.style.display = 'block';
+  } else if ((sum % 10) == 0 && cardNumber >= 16){
+    numberInput.classList.remove('invalid')
+    numberInput.classList.add('valid')
+    invalidInfo.style.display = 'none';
+  }
+}
 
 const mapCardNumber = str => {
   const html = str.split("").map((char, index) => {
@@ -24,6 +61,7 @@ const displayInput = e => {
       mapCardNumber(numberMask);
     } else {
       mapCardNumber(value);
+      validateCreditCardNumber(value)
     }
   } else if (e.target.name === "name") {
     cardName.textContent = value;
